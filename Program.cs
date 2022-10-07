c274b8d0225c178f2755a85a188b7b6e9b692104
RakVhalate <viktzajcev@mail.ru>
2022-10-07 12:53:55 +1000

    Добавил функцию отсутствия дежуранта.

--- Program.cs
+++ Program.cs
@@ -1,9 +1,10 @@
 ﻿using System;
-class Program
+class Chart
 {
+
     public static string[,] CreateWorkersList(int days)
     {
-        string[] imena = System.IO.File.ReadAllLines("namelist.txt");
+        string[] imena = System.IO.File.ReadAllLines("D:\\Programming\\CSharp\\ShiftsChart\\namelist.txt");
         int rows = imena.Length + 1; //количество рядов с поправкой на пустой нулевой ряд где находятся числа месяца
         int firstDay = DateTime.Now.Day;
         int columns = days + 2; //количество колонн с поправкой на нулевую колонну где находятся имена
@@ -18,49 +19,81 @@ class Program
             chart[0,i] = Convert.ToString(firstDay);
             if(i<10-DateTime.Now.Day) chart [0,i] += " "; //колонна с датами
             firstDay ++ ;
-        }
-        int maxChars = imena.Max(x => x.Length); //добавляем в нулевую ячейку элемент форматирования в соответствии с самым длинным именем
-        for(int m = 0; m <=maxChars - 1; m++)
-        {
-            chart[0,0] += " ";
-        }
-        for(int r = 0; r < rows; r++)  //выравниваем ячейки с именами до одной длины пробелами
-        {
-            while( chart[r,0].Length < maxChars)
-            {
-                chart[r,0] += " ";
-            }
-        }
-
-        
+        }       
         return chart;
     }
         public static string[,] Shifts(string [,] chart) //заполняем таблицу
     {
         int rows = chart.GetUpperBound(0) + 1;
         int columns = chart.Length / rows;
-        for(int j = 1; j < rows; j++) //ставим везде нули
+        for(int j = 1; j < rows; j++) //ставим везде нули для работы с таблицей
         {
             for(int i = 1; i < columns; i++)
             {
                 chart[j,i] = "0 ";
             }
         }
-            for(int j = 1; j < columns ;) //ставим дни дежурств
+        return chart;
+    }
+    public static string[,] IsAbsent(string[,] toPrint)
+    {
+        Console.WriteLine("В случае отсутствия человека введите его имя, затем через пробел число, с которого он отсутствует, затем на сколько дней он отсутствует");
+        string inp = Console.ReadLine();
+        string[] input = inp.Split(' ');
+        int rows = toPrint.GetUpperBound(0) + 1;
+        int columns = toPrint.Length / rows;
+        int abs = 0;
+        for(int i = 1; i < rows; i++)
+        {
+            if(toPrint[i,0] == input[0])
+            {
+                Console.WriteLine("Дежурный " + toPrint[i,0] + " будет отсутствовать с " + input[1] + " числа " + input[2] + " дней. Прогульщик!"); //ищем порядковый номер отсутствующего
+                abs = i;
+            }
+        }
+        for(int j = int.Parse(input[1]) - (DateTime.Now.Day - 1); j < int.Parse(input[1]) - (DateTime.Now.Day - 1) + int.Parse(input[2]); j++)
+                {
+                    toPrint[abs,j] = "Н ";
+                }
+        return toPrint;
+        
+    }
+    public static string[,] PutShifts(string[,] toShifts) //проставляем дежурства в зависимости от условий в таблице
+    {
+        int rows = toShifts.GetUpperBound(0) + 1;
+        int columns = toShifts.Length / rows;
+
+      for(int j = 1; j < columns ;) //ставим дни дежурств
         {
             for (int i = 1; i <rows & j <columns; i++)
             {
-                chart[i,j] = "Д ";
+                if (toShifts[i,j] == "Н ") i++;
+                toShifts[i,j] = "Д ";
                 j++;
             }
       
         }
-        return chart;
+      return toShifts;
+
+
     }
-    public static void DrawArray(string[,] toPrint)
+    public static void DrawArray(string[,] toPrint)  //метод, рисующий таблицу
     {
         int drawRows = toPrint.GetUpperBound(0) + 1;  //ряд с датами
         int drawColumns = toPrint.Length / drawRows; //ряд с именами
+        string[] imena = System.IO.File.ReadAllLines("D:\\Programming\\CSharp\\ShiftsChart\\namelist.txt");
+        int maxChars = imena.Max(x => x.Length); //добавляем в нулевую ячейку элемент форматирования в соответствии с самым длинным именем
+        for(int m = 0; m <= maxChars - 1; m++)
+        {
+            toPrint[0,0] += " ";
+        }
+        for(int r = 0; r < drawRows; r++)  //выравниваем ячейки с именами до одной длины пробелами
+        {
+            while( toPrint[r,0].Length < maxChars)
+            {
+                toPrint[r,0] += " ";
+            }
+        }
         
         for(int i = 0; i < drawRows; i++)
         {
@@ -76,8 +109,8 @@ class Program
     {
         {
         int nowDay = DateTime.Now.Day;
- //       int leftDays = DateTime.DaysInMonth(DateTime.Now.Year, DateTime.Now.Month) - nowDay;
         DrawArray(Shifts(CreateWorkersList(DateTime.DaysInMonth(DateTime.Now.Year, DateTime.Now.Month) - nowDay)));
+        DrawArray(PutShifts(IsAbsent(Shifts(CreateWorkersList(DateTime.DaysInMonth(DateTime.Now.Year, DateTime.Now.Month) - nowDay)))));
         Console.ReadLine();
         
         }
